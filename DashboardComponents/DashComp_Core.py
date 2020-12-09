e6621c5c71ec17fb5abdb2ec227d31095fb3d73e
Abhishek Roy <abhiroyq1@gmail.com>
2020-12-09 16:13:51 +0530

    Add files via upload

---
+++ DashboardComponents/DashComp_Core.py
@@ -0,0 +1,38 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+
+app = dash.Dash()
+
+app.layout = html.Div([
+                        html.Label('DropDown'),
+                        dcc.Dropdown(
+                            options=[{
+                                'label': 'New York City',
+                                'value': 'NYC'
+                            },{
+                                'label':'San Fransisco',
+                                'value':'SF'
+                            }],
+                            value='SF'
+                        ),
+                        html.Label('Slider'),
+                        dcc.Slider(min=-10,
+                                   max=10,
+                                   step=0.5,
+                                   value=0,
+                                   marks={i: str(i) for i in range(-10,10)}),
+                        html.Label('Some Radio items'),
+                        dcc.RadioItems(options=[{
+                                'label': 'New York City',
+                                'value': 'NYC'
+                            },{
+                                'label':'San Fransisco',
+                                'value':'SF'
+                        }],
+                        value='SF')
+])
+
+if __name__=='__main__':
+    app.run_server(debug=True)
---
+++ DashboardComponents/DashComp_HTML1.py
@@ -0,0 +1,25 @@
+import dash
+import dash_html_components as html
+
+app = dash.Dash()
+
+app.layout = html.Div(['This is the outermost Div!',
+                       html.Div(['This is the inner Div!'],
+                                style={
+                                    'color':'red',
+                                    'border':'2px red solid'
+                                }),
+                        html.Div(['Another inner Div!!'],
+                                 style={
+                                     'color':'blue',
+                                     'border':'2px blue solid'
+                                 })],
+                      style={
+                          'color':'green',
+                          'border':'2px green solid',
+                      })
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
