4898411db3a86f29fcdd2985379936f9f373f75d
Abhishek Roy <abhiroyq1@gmail.com>
2020-12-09 21:33:43 +0530

    Add files via upload

---
+++ InteractingWithVisualizations/1_HoverOverData.py
@@ -0,0 +1,77 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
+import json
+import base64
+
+
+app = dash.Dash()
+
+df = pd.read_csv('../../Plotly-Dashboards-with-Dash-master/Data/wheels.csv')
+
+def encode_image(image_file):
+    encoded = base64.b64encode(open(image_file, 'rb').read())
+    return 'data:image/png;base64,{}'.format(encoded.decode())
+
+# app.layout = html.Div([
+#                 html.Div(dcc.Graph(id='wheels-plot',
+#                                    figure={
+#                                        'data':[go.Scatter(
+#                                            x=df['color'],
+#                                            y=df['wheels'],
+#                                            dy=1,
+#                                            mode='markers',
+#                                            marker={'size':15}
+#                                        )],
+#                                        'layout':go.Layout(
+#                                            title='test',
+#                                            hovermode='closest'
+#                                        ),
+#                                    }),
+#                                     style={'width':'30%', 'float':'left'}),
+#                 html.Div(html.Pre(id='hover-data', style={'paddingTop':35}),
+#                          style={'width':'30%'}),
+#                 html.Div(html.Img(id='display-image', src='children', height=300))
+# ])
+
+app.layout = html.Div([
+                html.Div(dcc.Graph(id='wheels-plot',
+                                   figure={
+                                       'data':[go.Scatter(
+                                           x=df['color'],
+                                           y=df['wheels'],
+                                           dy=1,
+                                           mode='markers',
+                                           marker={'size':15}
+                                       )],
+                                       'layout':go.Layout(
+                                           title='test',
+                                           hovermode='closest'
+                                       ),
+                                   }),
+                                    style={'width':'30%', 'float':'left'}),
+                html.Div([html.Img(id='hover-data', src='children', height=300)],
+                         style={'paddingTop':35
+                                })
+])
+
+# @app.callback(Output('hover-data', 'children'),
+#               [Input('wheels-plot', 'hoverData')])
+# def callback_image(hoverData):
+#     return json.dumps(hoverData, indent=2)
+
+@app.callback(Output('hover-data', 'src'),
+              [Input('wheels-plot', 'hoverData')])
+def callback_image(hoverData):
+    wheel= hoverData['points'][0]['y']
+    color = hoverData['points'][0]['x']
+    path = r"../../Plotly-Dashboards-with-Dash-master/Data/Images/"
+    return encode_image(path + df[(df['wheels'] == wheel) & (df['color'] == color)]['image'].values[0])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
