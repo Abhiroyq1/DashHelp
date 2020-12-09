8ba01eb2d580d7f9bb51db40e491b68551ebfad5
Abhishek Roy <abhiroyq1@gmail.com>
2020-12-09 21:31:14 +0530

    Add files via upload

--- InteractiveComponents/5_Exercise.py
+++ InteractiveComponents/5_Exercise.py
@@ -15,13 +15,18 @@ app.layout = html.Div([
         min=-5,
         max=6,
         step=1,
-        value=[-2, 3],
-        marks=[{
-            str(i): i
-        } for i in range(-5,7)]
+        value=[-1, 1],
+        marks={
+            i: str(i) for i in range(-10,11)
+        }
     ),
-    html.Div(id='output-container-range-slider', style={'fontSize':10})
+    html.H1(id='Product')
 ])
+
+@app.callback(Output(component_id='Product', component_property='children'),
+             [Input(component_id='my-range-slider', component_property='value')])
+def update_output_div(input_value):
+    return str(int(input_value[0])*int(input_value[1]))
 
 if __name__=='__main__':
     app.run_server(debug=True)
