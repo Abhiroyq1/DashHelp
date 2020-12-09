d3de9ff70a587c4ce932f3d9a5de3fd68728b9a1
Abhishek Roy <abhiroyq1@gmail.com>
2020-12-09 21:34:10 +0530

    Add files via upload

---
+++ ControllingCallbacks/ControllingCallbacks.py
@@ -0,0 +1,28 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+
+
+app = dash.Dash()
+
+app.layout= html.Div([
+                dcc.Input(id='number-in', value=1, style={'fontSize':24}),
+                html.Button(id='submit-button',
+                            n_clicks=0,
+                            children='Submit Here',
+                            style={
+                                'fontSize':24
+                            }),
+                html.H1(id='number-out')
+])
+
+
+@app.callback(Output('number-out', 'children'),
+              [Input('submit-button', 'n_clicks')],
+              [State('number-in', 'value')])
+def output(n_clicks, number):
+    return f"{number} was clicked in and button was clicked {n_clicks} times."
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
